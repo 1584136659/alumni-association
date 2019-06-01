568c465e0608dfa9c06fa2908d9bc5b653b32085
1584136659 <1584136659@qq.com>
2019-06-01 22:55:24 +0800

    first

---
+++ src/main/java/com/cafucaa/alumniassociation/AlumniAssociationApplication.java
@@ -0,0 +1,13 @@
+package com.cafucaa.alumniassociation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniAssociationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlumniAssociationApplication.class, args);
+    }
+
+}
